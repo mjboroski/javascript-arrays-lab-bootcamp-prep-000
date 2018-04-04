39b387342e8ccc5289141715a2e8b062e014de39
Michael Boroski <mjboroski@gmail.com>
2018-04-04 18:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = "I don't do much."
 kittens= ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){
-  kittens=push.kittens(name);
+  kittens.push(name);
   return kittens;
 }
 
